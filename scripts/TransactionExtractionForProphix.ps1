6084bc19585327f909eb057154ee06694219c0d1
Benjamin Whitney <benjamin.whitney@amquipinc.com>
2024-11-27 18:31:29 -0500

    Init commit

---
+++ scripts/TransactionExtractionForProphix.ps1
@@ -0,0 +1,17 @@
+Import-Module ".\src\PostgreSQLUtils" -Force
+
+$CompanyFilePath = Get-Content ".\CompanyFIlePaths.json" -Raw | ConvertFrom-Json
+$Divisions = New-Object System.Collections.ArrayList
+$Divisions.Clear()
+
+# Loop through each property name and add it to $Divisions individually
+foreach ($divisionName in $CompanyFilePath.PSObject.Properties.Name) {
+    $Divisions.Add($divisionName) | Out-Null
+}
+
+# Output the list
+$Divisions
+
+foreach ($div in $Divisions) {
+    $CompanyFilePath.$div
+}
