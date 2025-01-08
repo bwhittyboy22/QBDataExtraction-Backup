f8eca48f55212b0586992bf3ed8da93d3aa1c00a
Benjamin Whitney <benjamin.whitney@amquipinc.com>
2025-01-08 01:16:59 -0500

    Can query transaction table with modified date filter.

--- scripts/queries/ECH-TransactionDateFilter.ps1
+++ scripts/queries/ECH-TransactionDateFilter.ps1
@@ -1,18 +1,53 @@
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$FromDate,
+    [string]$ToDate = (Get-Date -Format "yyyy-MM-dd")
+)
+
+
+# Validate and convert FromDate
+try {
+  $FromDateTime = [DateTime]::ParseExact($FromDate, "yyyy-MM-dd", $null)
+  $FromDate = $FromDateTime.ToString("yyyy-MM-ddT00:00:00")
+} catch {
+  throw "FromDate must be in the format 'YYYY-MM-DD'"
+}
+
+# Handle ToDate - if provided, validate it. If not, use current date and time
+try {
+  $currentDate = Get-Date
+  if ($ToDate) {
+      $ToDateTime = [DateTime]::ParseExact($ToDate, "yyyy-MM-dd", $null)
+      # If ToDate is today, use current time
+      if ($ToDateTime.Date -eq $currentDate.Date) {
+          $ToDateTime = $currentDate
+      }
+      # If ToDate is before today, use end of day (23:59:59)
+      elseif ($ToDateTime.Date -lt $currentDate.Date) {
+          $ToDateTime = $ToDateTime.Date.AddHours(23).AddMinutes(59).AddSeconds(59)
+      }
+  } else {
+      # No ToDate provided, use current date and time
+      $ToDateTime = $currentDate
+  }
+  $ToDate = $ToDateTime.ToString("yyyy-MM-ddTHH:mm:ss")
+} catch {
+  throw "ToDate must be in the format 'YYYY-MM-DD'"
+}
+
 # Create instance of COM object
 $qbxmlrp = New-Object -ComObject QBXMLRP2.RequestProcessor
 
-# Path to QB file
+# Path to QB company file
 $companyFile = "C:\QB Files\ECH\NEW Eastern Crane & Hoist, Inc.QBW"
 
-$fromDate   = "2024-11-17T00:00:00"
-$toDate     = "2024-11-17T23:59:59"
 $StringToPrint = @"
-From date: $fromDate, To date: $toDate
+From date: $FromDate, To date: $ToDate
 "@
 Write-Host $StringToPrint
 
+# Open connection to QuickBooks
 try {
-  # Open connection to QuickBooks
   $qbxmlrp.OpenConnection2("","ECHQBAutomation", 1)
   $connectionStatus = "Connection successful"
   Write-Output $connectionStatus
@@ -22,7 +57,14 @@ try {
 }
 
 # Begin a session
-$ticket = $qbxmlrp.BeginSession($companyFile, 2)
+try {
+  $ticket = $qbxmlrp.BeginSession($companyFile, 2)
+  $sessionStatus = "Session sucessful"
+  Write-Output $sessionStatus
+} catch {
+  $sessionStatus = "Session not established"
+  Write-Output $sessionStatus
+}
 
 # QBXML request
 $qbxmlRequest = @"
@@ -31,8 +73,8 @@ $qbxmlRequest = @"
     <QBXMLMsgsRq onError="continueOnError">
         <TransactionQueryRq requestID="2">
                 <TransactionModifiedDateRangeFilter>
-                    <FromModifiedDate>$fromDate</FromModifiedDate>
-                    <ToModifiedDate>$toDate</ToModifiedDate>
+                    <FromModifiedDate>$FromDate</FromModifiedDate>
+                    <ToModifiedDate>$ToDate</ToModifiedDate>
                 </TransactionModifiedDateRangeFilter>
         </TransactionQueryRq>
     </QBXMLMsgsRq>
@@ -42,23 +84,20 @@ $qbxmlRequest = @"
 # Try to send the request and catch any errors
 try {
     # Send the request
+    Write-Output "Sending query..."
     $response = $qbxmlrp.ProcessRequest($ticket, $qbxmlRequest)
+    Write-Output "Query received"
     
     # Save the response if successful
     # Get the current date and time
     $currentDateTime_FolderPath = Get-Date -Format "yyyyMMdd"
     $currentDateTime_FileName = Get-Date -Format "yyyyMMdd_HHmm"
     
-    # Path to Documents folder
     $documentsPath = [System.Environment]::GetFolderPath("MyDocuments")
-    
-    # Create the subfolder with the current date and time
     $subfolderPath = Join-Path -Path $documentsPath -ChildPath "QBFileExports\$currentDateTime_FolderPath"
     if (-not (Test-Path -Path $subfolderPath)) {
         New-Item -ItemType Directory -Path $subfolderPath | Out-Null
     }
-    
-    # Create the file path with the date prefix
     $filePath = Join-Path -Path $subfolderPath -ChildPath "Transactions_ECH_${currentDateTime_FileName}.xml"
     
     # Save the response to the file
